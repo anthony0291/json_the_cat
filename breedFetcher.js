35a4f8d3b16f486aef897d59500bd8029c17e513
bzrkrbddy081 <bzrkrbddy081@gmail.com>
2023-06-11 23:51:57 +0000

    trying to debugg

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,22 @@
+
 const request = require('request');
+const url = `https://api.thecatapi.com/v1/breeds/search?q=`;
+
+const fetchBreedDescription = (breedName, callback) => {
+  request(`${url}${breedName}`, (error, response, body) => {
+  if (error) {
+    callback(error, null);
+  }
+  const data = JSON.parse(body);
+  console.log(data[0]);
+  if (data.length < 1) {
+      error = `fail`;
+      return callback(error, null);
+    }
+      return callback(null, data[0].description.trim());
+    });
+  };
+
+  module.exports = { fetchBreedDescription };
+
+
---
+++ index.js
@@ -0,0 +1,12 @@
+const fetchBreed = require('./breedFetcher');
+const breedName = process.argv[2];
+
+
+
+fetchBreed(breedName, (error, description) => {
+  if (error) {
+    return console.log(`Failed: ${error}`);
+  } else {
+    return console.log(description);
+  }
+});
